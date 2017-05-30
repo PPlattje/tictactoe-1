a878dbba3de0f8a06e1a96a2a0712d00b359a4a8
PPlattje <patrickplattje@outlook.com>
2017-05-30 18:02:46 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+
+}
+?>
+<table>
+	<tr>
+		<td><?=$position1?></td>
+		<td><?=$position2?></td>
+		<td><?=$position3?></td>	
+	</tr>
+	<tr>
+		<td><?=$position4?></td>
+		<td><?=$position5?></td>
+		<td><?=$position6?></td>
+	</tr>
+	<tr>
+		<td><?=$position7?></td>
+		<td><?=$position8?></td>
+		<td><?=$position9?></td>
+	</tr>	
+</table>
+   <script src="main3.js"></script>
+  </body>
+</html>
